7694ceb57f201eb46827042bb0ec5c537c5dc0ed
Alexander K <alexander@Alexanders-MacBook-Air.local>
2018-04-15 13:32:22 +0200

    added X image and manually import DGElasticPullToRefresh instead of pods

---
+++ todoApp/Extensions/UIImageExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIImageExtensions.swift
+//  todoApp
+//
+//  Created by Alexander K on 15/04/2018.
+//  Copyright © 2018 Alexander Kvamme. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIImage {
+    static var xmarkIcon: UIImage {
+        return UIImage(named: "xmark")!
+    }
+}
+
